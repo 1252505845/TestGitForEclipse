1edc4d99956a1538d2cc58654bbdffb0ab44d87c
tom_glb <goodMorning_pro@atguigu.com>
2020-06-02 21:05:36 +0800

    uu1

--- src/main/java/com/atguigu/config/MyConfig.java
+++ src/main/java/com/atguigu/config/MyConfig.java
@@ -6,22 +6,7 @@ public class MyConfig {
 
 	System.out.println("dev");
 
-	System.out.println("ÂµÃšÂ¶Ã¾Â´ÃŽ");
-	System.out.println();
-       System.out.println("asasaa");
-	System.out.println();
-       System.out.println("ËæÊ±ËæµØ");
 
-      
-	System.out.println();
-       System.out.println("asasaa");
-       
-       
-       
-   	System.out.println();
-    System.out.println("asasaa");
-	System.out.println();
-    System.out.println("ËæÊ±ËæµØ");
 
 }
 }
